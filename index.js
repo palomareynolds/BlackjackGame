7d5347bf75266fce429e9032ea068a534e405b28
Paloma Reynolds <palomap.paula@hotmail.com>
2023-07-03 21:56:02 -0400

    feat: addCSS

---
+++ index.js
@@ -0,0 +1,21 @@
+let firstCard = 6;
+let secondCard = 9;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+
+if (sum <= 20) {
+    messa = ("Do you want to draw a new card?")
+} else if (sum === 21) {
+    message = ("Wohoo! You've got Blackjack!")
+    hasBlackJack = true;
+} else {
+    message = ("You're out of the game!")
+    isAlive = false;
+}
+
+console.log(isAlive);
+console.log("");
+
