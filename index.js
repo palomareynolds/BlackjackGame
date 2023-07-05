cc5c0220d9199fda79477a20fca6a1908f961e12
Paloma Reynolds <palomap.paula@hotmail.com>
2023-07-05 19:13:40 -0400

    refactor: font-size

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+
+
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum += firstCard + secondCard;
+
+
     renderGame();
 }
 
@@ -35,16 +43,24 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log("Drawing a new card.")
-    let card = getRandomCard();
-    cards.push(card);
-    sum += card;
-    renderGame();
+    if (isAlive && hasBlackJack === false) {
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
+    }
+    
 }
 
 function getRandomCard() {
-    let randomCard = Math.floor(Math.random()* 13) + 1;
-    return randomCard;
+    let randomNumber = Math.floor(Math.random()* 13) + 1;` `
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    }
 }
     
 
